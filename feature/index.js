25591ef1f6379d40ae00d7f166965b563dbbbad6
Barista Clarista Doe <thesmoremail@gmail.com>
2018-12-28 23:11:18 -0800

    a

--- feature/index.js
+++ feature/index.js
@@ -3,4 +3,5 @@ var article = [
  [0,'2018-12-26','mesothelioma','Mesothelioma','Bringing Lawyers and Plantifs together since 1972.']
 ,[0,'2018-12-26','insurance','Insurance','What ails ya, coasts ya.']
 ,[0,'2018-12-26','utility','Utility','Gas and Electric - heat and light since 1892.']
+,[0,'2018-12-26','mortgage','Mortgage','Mortgage - Death, Taxes and Mortgages - in that order']
 ];
