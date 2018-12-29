a6955cb001a3c1754d93b58ac6d8914dcf5b9e94
Barista Clarista Doe <thesmoremail@gmail.com>
2018-12-29 14:28:39 -0800

    a

--- feature/index.js
+++ feature/index.js
@@ -4,5 +4,6 @@ var article = [
 ,[0,'2018-12-26','insurance','Insurance','What ails ya, coasts ya.']
 ,[0,'2018-12-26','utility','Utility','Gas and Electric - heat and light since 1892.']
 ,[0,'2018-12-26','mortgage','Mortgage','Mortgage - Death, Taxes and Mortgages - in that order']
+,[0,'2018-12-27','attorney','attorney','Attorney - Cant live with them, cant live with them']
 
 ];
