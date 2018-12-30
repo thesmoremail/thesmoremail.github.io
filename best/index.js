99470d081ac691f53896188f60dd817a6735669a
Barista Clarista Doe <thesmoremail@gmail.com>
2018-12-30 11:47:54 -0800

    a

--- best/index.js
+++ best/index.js
@@ -2,7 +2,7 @@
 //featured, date, folder, title, description,thumbnail
 var article = [
  [0,'2018-12-26','mesothelioma','Mesothelioma','Bringing Lawyers and Plantifs together since 1972.','']
-,[0,'2018-12-26','insurance','Insurance','What ails ya, coasts ya.','']
+,[0,'2018-12-26','insurance','Insurance','What ails ya, costs ya.','']
 ,[0,'2018-12-26','utility','Utility','Gas and Electric - heat and light since 1892.','']
 ,[0,'2018-12-26','mortgage','Mortgage','Death, Taxes and Mortgages - in that order','']
 ,[0,'2018-12-27','book-dracula','Dracula Book','A classic tail of being out for blood.','']
