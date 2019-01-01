59dfb06a08959dd81240964588b33727bdc656e3
thesmoremail <thesmoremail@gmail.com>
2018-12-31 21:46:27 -0800

    msg

--- best/index.js
+++ best/index.js
@@ -14,7 +14,7 @@ var article = [
 ,[0,'2018-12-30','degree','Degrees',"Edumication - it's not just for Nerd's anymore",'']
 ,[0,'2018-12-30','clickgame','ClickGame',"get your mouse or trackball.  this one moves pretty fast.",'']
 ,[0,'2018-12-31','chat','Chat',"When you need to ask for help on how to toast your sMoreMail, ask these bots.",'']
-,[0,'2018-12-31','email','Email',"Shenanigan Protection;  Methods, Tests, and Techniques for email phishing, farming, and spoofing identification and sanitizaion.",'']
+,[0,'2018-12-31','emails','Email',"Shenanigan Protection;  Methods, Tests, and Techniques for email phishing, farming, and spoofing identification and sanitizaion.",'']
 
 ];
 
