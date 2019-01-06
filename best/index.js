33dc8f18660b70aaf73237302a2d659e6830fc13
Barista Clarista Doe <thesmoremail@gmail.com>
2019-01-05 22:14:53 -0800

    a

--- best/index.js
+++ best/index.js
@@ -21,6 +21,7 @@ var article = [
 ,[0,'2019-01-03','dev','△ Development',"The pyramid of power for the best development cycles",'']
 ,[0,'2019-01-03','asbestos_map','🌎 Asbestos Map',"If You don't find it, you can't breath the stuff!",'']
 ,[0,'2019-01-04','gold','🤑 Gold',"They Told you where to look, but you didn't believe them",'']
+,[0,'2019-01-04','paranoidbible','😶 Paranoid\'s Bible',"Cyber Stocking and Anti-Doxing, Anonymous Tumblr authors bring you da links to check",'']
 
 ];
 
