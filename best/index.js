32a8296dd88c11f335b28f875b3a9956eac938ad
Barista Clarista Doe <thesmoremail@gmail.com>
2019-01-05 23:13:14 -0800

    a

--- best/index.js
+++ best/index.js
@@ -22,7 +22,7 @@ var article = [
 ,[0,'2019-01-03','asbestos_map','🌎 Asbestos Map',"If You don't find it, you can't breath the stuff!",'']
 ,[0,'2019-01-04','gold','🤑 Gold',"They Told you where to look, but you didn't believe them",'']
 ,[0,'2019-01-04','paranoidbible','😶 Paranoid\'s Bible',"Cyber Stocking and Anti-Doxing, Anonymous Tumblr authors bring you da links to check",'']
-,[0,'2019-01-04','asbestos_trusts2','😶 Asbestos Trusts links',"The plot is now friable and airborn, PPE is missing",'']
+,[0,'2019-01-04','asbestos_trusts2','🤮 Asbestos Trusts links',"The plot is now friable and airborn, PPE is missing",'']
 
 ];
 
